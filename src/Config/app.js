6c0d8bf38a84f7d635c43cb2313891b6eb025427
Mohamadreza Rezaei <me.moham6dreza@gmail.com>
2024-10-26 22:59:14 +0330

    add ItemApiService.js

---
+++ src/Config/app.js
@@ -0,0 +1,5 @@
+const config = {
+    apiUrl: import.meta.env.VITE_BACKEND_API_SERVER_URL
+}
+
+export default config
---
+++ src/Services/Item/ItemApiService.js
@@ -0,0 +1,61 @@
+import axios from 'axios';
+import config from "../../Config/app.js";
+
+class ItemApiService {
+    static API_BASE_URL = config.apiUrl;
+
+    static getFullUrl(endpoint) {
+        return `${ItemApiService.API_BASE_URL}/${endpoint}`;
+    }
+
+    // Create
+    static async createItem(data) {
+        try {
+            const response = await axios.post(self.getFullUrl('items'), data);
+            return response.data;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    // Read
+    static async getItems() {
+        try {
+            const response = await axios.get(self.getFullUrl('items/index'));
+            return response.data;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    static async getItemById(itemId) {
+        try {
+            const response = await axios.get(self.getFullUrl(`items/${itemId}`));
+            return response.data;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    // Update
+    static async updateItem(itemId, data) {
+        try {
+            const response = await axios.put(self.getFullUrl(`items/${itemId}`), data);
+            return response.data;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    // Delete
+    static async deleteItem(itemId) {
+        try {
+            const response = await axios.delete(self.getFullUrl(`items/${itemId}`));
+            return response.data;
+        } catch (error) {
+            throw error;
+        }
+    }
+}
+
+export default ItemApiService;
