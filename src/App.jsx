405cf582cbc6b246a96ceffea22a1dec3493cfaf
Mohamadreza Rezaei <me.moham6dreza@gmail.com>
2024-10-27 23:43:38 +0330

    add spinner when fetch items

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,17 @@ import {Masonry} from "./Components/Layouts/Masonry.jsx";
 import {SwiperList} from "./Components/Item/Carousels/SwiperList.jsx";
 import {GridList} from "./Components/Item/Carousels/GridList.jsx";
 import {useEffect} from "react";
-import {fetchItems, selectStatus} from "./Features/Item/item.slice.js";
+import {fetchItems, selectError, selectStatus} from "./Features/Item/item.slice.js";
 import {useDispatch, useSelector} from "react-redux";
+import {Spinner} from "./Components/Partials/Spinner.jsx";
+import {E404} from "./Components/Error/E404.jsx";
 
 function App() {
 
     const dispatch = useDispatch()
 
     const status = useSelector(selectStatus)
+    const error = useSelector(selectError)
 
     useEffect(() => {
         if (status === 'idle') {
@@ -18,12 +21,32 @@ function App() {
         }
     }, [status]);
 
+    let content
+
+    switch (status) {
+        case 'pending':
+            content = <Spinner/>
+            break
+        case 'completed':
+            content = <div>
+                <SwiperList/>
+                <GridList/>
+            </div>
+            break
+        case 'failed':
+            content = <div>{error}</div>
+            break
+        default:
+            content = <E404/>
+    }
+
     return (
         <>
             <Hero/>
             <Masonry/>
-            <SwiperList/>
-            <GridList/>
+            {
+                content
+            }
         </>
     )
 }
---
+++ src/Components/Partials/Spinner.jsx
@@ -0,0 +1,24 @@
+import '../../App.css'
+import logo from '../../Assets/logo.svg';
+
+export const Spinner = ({text, size}) => {
+    return (
+        <>
+            <div className="dark:bg-slate-900 bg-white max-w-[80rem] flex flex-col mx-auto size-full">
+
+                {/*<!-- ========== MAIN CONTENT ========== -->*/}
+                <main id="content">
+                    <div className="text-center py-10 px-4 sm:px-6 lg:px-8">
+                        <div className="mt-5 flex flex-col justify-center items-center gap-2 sm:flex-row sm:gap-3">
+                            <img src={logo} className="App-logo" alt="logo"/>
+                            <h1 className={'text-2xl font-bold capitalize mt-10'}>
+                                {text}
+                            </h1>
+                        </div>
+                    </div>
+                </main>
+                {/*<!-- ========== END MAIN CONTENT ========== -->*/}
+            </div>
+        </>
+    )
+}
